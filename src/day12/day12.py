fb01a11f7cac9043cf7e6f400c8f6cc34a5a5110
Cam Warner <cameroncwarner@gmail.com>
2019-12-12 00:54:59 -0500

    Refactor day 12 part 1

--- src/day12/day12.py
+++ src/day12/day12.py
@@ -3,38 +3,41 @@ from itertools import combinations
 from functools import reduce
 import operator
 
-test = False
-part1 = True
-part2 = False
+test = True
+part1 = False
+part2 = True
 
-def Part1(lines):
+def readMoons(lines):
     moons = []
     for line in lines:
         values = [int(i.split('=')[1].strip('>\n')) for i in line.split(',')]
         moons.append([values, [0, 0 ,0]])
-    
-    for i in range(1000):
-        # apply gravity
-        for moon1, moon2 in combinations(moons, 2):
-            moon1Pos, moon1Vel = moon1
-            moon2Pos, moon2Vel = moon2
-            for axis in range(3):
-                oneChange = 0
-                twoChange = 0
-                if moon1Pos[axis] > moon2Pos[axis]:
-                    oneChange = -1
-                    twoChange = 1
-                elif moon1Pos[axis] < moon2Pos[axis]:
-                    oneChange = 1
-                    twoChange = -1
-                moon1Vel[axis] += oneChange
-                moon2Vel[axis] += twoChange
-        
-        # apply velocity
-        for moon in moons:
-            moon[0] = list(map(operator.add, moon[0], moon[1]))
-            #print(moon)
-    
+    return moons
+
+def applyGravity(moons):
+    # apply gravity
+    for moon1, moon2 in combinations(moons, 2):
+        moon1Pos, moon1Vel = moon1
+        moon2Pos, moon2Vel = moon2
+        for axis in range(3):
+            oneChange = 0
+            twoChange = 0
+            if moon1Pos[axis] > moon2Pos[axis]:
+                oneChange = -1
+                twoChange = 1
+            elif moon1Pos[axis] < moon2Pos[axis]:
+                oneChange = 1
+                twoChange = -1
+            moon1Vel[axis] += oneChange
+            moon2Vel[axis] += twoChange
+
+def applyVelocity(moons):
+    # apply velocity
+    for moon in moons:
+        moon[0] = list(map(operator.add, moon[0], moon[1]))
+        #print(moon)
+
+def totalEnergy(moons):
     # calc total energy
     total = 0
     for moon in moons:
@@ -43,11 +46,20 @@ def Part1(lines):
         subTotal = potential * kinetic
         total += subTotal
     print('total energy=', total)
+    return total
 
-        
+def Part1(lines):
+    moons = readMoons(lines)
+    
+    for i in range(1000):
+        applyGravity(moons)
+        applyVelocity(moons)
+    
+    totalEnergy(moons)    
 
 def Part2(lines):
     pass
+    
 
 file = "input.txt"
 
