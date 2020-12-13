3c5c4ed0603acc46e741d63677737018f76e2e87
Cam Warner <cameroncwarner@gmail.com>
2020-12-12 21:26:34 -0500

    Finish day 11 2020

--- src/2020/day11/day11.py
+++ src/2020/day11/day11.py
@@ -1,11 +1,11 @@
 import common as com
 
 test = False
-part1 = True
-part2 = False
+part1 = False
+part2 = True
 puzzle = com.PuzzleWithTests()
 
-def getOccupiedSeatCount(grid: com.CartesianGrid, point: com.Point):
+def getOccupiedSeatCountPart1(grid: com.CartesianGrid, point: com.Point):
     count = 0
     for x in range(point.x - 1, point.x + 2):
         for y in range(point.y - 1, point.y +2):
@@ -16,17 +16,54 @@ def getOccupiedSeatCount(grid: com.CartesianGrid, point: com.Point):
                 count += 1
     return count
 
-def getOldAndNewSeat(grid: com.CartesianGrid, x, y):
+def getOccupiedSeatCountPart2(grid: com.CartesianGrid, point: com.Point):
+    count = 0
+    upLeft = [-1, 1]
+    upRight = [1, 1]
+    downLeft = [-1, -1]
+    downRight = [1, -1]
+    
+    directions = (
+        upLeft,
+        com.CartesianGrid.UP,
+        upRight,
+        com.CartesianGrid.RIGHT,
+        downRight,
+        com.CartesianGrid.DOWN,
+        downLeft,
+        com.CartesianGrid.LEFT
+    )
+
+    for direction in directions:
+        adjPoint = point
+        while adjPoint is not None:
+            adjPoint = grid.getPoint(*(adjPoint + direction))
+            if adjPoint is None:
+                break
+            if adjPoint.data == '#':
+                count += 1
+                break
+            if adjPoint.data == 'L':
+                break
+    return count
+
+def getOldAndNewSeatPart1(grid: com.CartesianGrid, x, y):
     point:com.Point = grid.getPoint(x, y)
-    if point.data == 'L' and getOccupiedSeatCount(grid, point) == 0:
+    if point.data == 'L' and getOccupiedSeatCountPart1(grid, point) == 0:
         return point.data, '#'
-    elif point.data == '#' and getOccupiedSeatCount(grid, point) >= 4:
+    elif point.data == '#' and getOccupiedSeatCountPart1(grid, point) >= 4:
+        return point.data, 'L'
+    return point.data, point.data
+
+def getOldAndNewSeatPart2(grid: com.CartesianGrid, x, y):
+    point:com.Point = grid.getPoint(x, y)
+    if point.data == 'L' and getOccupiedSeatCountPart2(grid, point) == 0:
+        return point.data, '#'
+    elif point.data == '#' and getOccupiedSeatCountPart2(grid, point) >= 5:
         return point.data, 'L'
     return point.data, point.data
     
-
-
-def Part1(lines):
+def runSimulation(lines, getOldAndNewSeatFcn):
     grid = com.CartesianGrid(' ')
     x = 0
     y = 0
@@ -44,16 +81,13 @@ def Part1(lines):
         hadChange = False
         for y in range(0, -1*len(lines), -1):
             for x in range(len(lines[0].strip())):
-                oldSeat, newSeat = getOldAndNewSeat(grid, x, y)
+                oldSeat, newSeat = getOldAndNewSeatFcn(grid, x, y)
                 newPoint = com.Point(x, y, newSeat)
                 hadChange |= oldSeat != newSeat
                 newGrid.addPoint(newPoint)
         grid = newGrid
         if not hadChange:
             break
-        print('------------------------------------')
-        print('')
-        print(grid)
         loops += 1
 
     count = 0
@@ -62,8 +96,12 @@ def Part1(lines):
             count += 1
     return count
 
+def Part1(lines):
+    return runSimulation(lines, getOldAndNewSeatPart1)
+
 def Part2(lines):
-    return None
+    return runSimulation(lines, getOldAndNewSeatPart2)
+
 
 if test:
     lines = com.readFile("test.txt")
