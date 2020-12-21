6b4e068340193d0707e4708e1417c29153b6977f
Cam Warner <cameroncwarner@gmail.com>
2020-12-21 13:09:27 -0500

    In progress on day 10 and 16 2020

--- src/2020/day10/day10.py
+++ src/2020/day10/day10.py
@@ -26,6 +26,54 @@ def Part1(lines):
         prev = adapter
     return oneGaps * (threeGaps+1)
 
+def runTest(numbersInARow: int):
+    for i in range(1, numbersInARow-2):
+        for j in range(1, numbersInARow-2):
+
+
+def getCombinations(oneDiffssInARow: int):
+    # 1
+    # 12
+    # 123
+    # 1_3
+    # 1234
+    # 1_34
+    # 1__4
+    # 12_4
+    # 12345
+    # 1_345
+    # 1__45
+    # 12_45
+    # 12__5
+    # 123_5
+    # 123456
+    # 1_3456
+    # 1__456
+    # 12_456
+    # 12__56
+    # 123_56
+    # 123__6
+    # 1234_6
+    # 1234567
+    # 1_34567
+    # 1__4567
+    # 12_4567
+    # 12__567
+    # 123_567
+    # 123__67
+    # 1234_67
+    # 1234__7
+    # 12345_7
+
+
+    frontAndBackConstant = 2
+    allThere = 1
+    combinationsWithoutSingleNumber = 2
+    numbersInThisChain = oneDiffssInARow + 1 # add one because there is one more number than diffs
+    if(numbersInThisChain < 3):
+        return 1
+    return ((numbersInThisChain - frontAndBackConstant) * combinationsWithoutSingleNumber) + allThere
+
 def Part2(lines):
     minVoltageGap = 1
     maxVoltageGap = 3
@@ -49,7 +97,7 @@ def Part2(lines):
             onesInARow += 1
         else:
             if onesInARow != 0:
-                total *= 2**(onesInARow-1)
+                total *= getCombinations(onesInARow)
                 onesInARow = 0
     return total
 
--- src/2020/day16/day16.py
+++ src/2020/day16/day16.py
@@ -1,15 +1,150 @@
+from typing import Set
 import common as com
 
-test = True
-part1 = True
-part2 = False
+test = False
+part1 = False
+part2 = True
 puzzle = com.PuzzleWithTests()
 
+def readTicket(line:str):
+    ticket = list()
+    for field in line.split(','):
+        number = int(field)
+        ticket.append(number)
+    return ticket
+
 def Part1(lines):
-    return None
+    rules = dict()
+    section = 0
+    myTicket = None
+    tickets = list()
+    for line in lines:
+        if line.strip() == '':
+            continue
+        if 'your ticket' in line:
+            section += 1
+            continue
+        elif 'nearby tickets' in line:
+            section += 1
+            continue
+        elif section == 0:
+            field, numbers = line.split(':')
+            range1, range2 = numbers.split('or')
+            range1Bot, range1Top = range1.split('-')
+            range2Bot, range2Top = range2.split('-')
+            rules[field] = ((int(range1Bot), int(range1Top)), (int(range2Bot), int(range2Top)))
+            continue
+        elif section == 1:
+            myTicket = readTicket(line)
+        elif section == 2:
+            tickets.append(readTicket(line))
+
+    rate = 0
+
+    for ticket in tickets:
+        for field in ticket:
+            passedRule = False
+            for rule in rules:
+                for set in rules[rule]:
+                    if set[0] <= field <= set[1]:
+                        passedRule = True
+                        break
+                if passedRule:
+                    break
+            if not passedRule:
+                    rate += field
+
+    return rate
 
 def Part2(lines):
-    return None
+    rules = dict()
+    section = 0
+    myTicket = None
+    tickets = list()
+    for line in lines:
+        if line.strip() == '':
+            continue
+        if 'your ticket' in line:
+            section += 1
+            continue
+        elif 'nearby tickets' in line:
+            section += 1
+            continue
+        elif section == 0:
+            field, numbers = line.split(':')
+            range1, range2 = numbers.split('or')
+            range1Bot, range1Top = range1.split('-')
+            range2Bot, range2Top = range2.split('-')
+            rules[field] = ((int(range1Bot), int(range1Top)), (int(range2Bot), int(range2Top)))
+            continue
+        elif section == 1:
+            myTicket = readTicket(line)
+        elif section == 2:
+            tickets.append(readTicket(line))
+
+
+    goodTickets = list()
+    for ticket in tickets:
+        ticketIsGood = True
+        for field in ticket:
+            passedRule = False
+            for rule in rules:
+                for ruleSet in rules[rule]:
+                    if ruleSet[0] <= field <= ruleSet[1]:
+                        passedRule = True
+                        break
+                if passedRule:
+                    break
+            if not passedRule:
+                ticketIsGood = False
+        if ticketIsGood:
+            goodTickets.append(ticket)
+
+    ruleToFieldsIdx = dict()
+    for rule in rules:
+        goodFields = set()
+        for field in range(len(tickets[0])):
+            goodFields.add(field)
+
+        for ticket in goodTickets:
+            idx = 0
+            for field in ticket:
+                if idx not in goodFields:
+                    idx += 1
+                    continue
+                isGood = False
+                for ruleSet in rules[rule]:
+                    if ruleSet[0] <= field <= ruleSet[1]:
+                        isGood = True
+                if not isGood:
+                    goodFields.remove(idx)
+                idx += 1
+        ruleToFieldsIdx[rule] = goodFields
+
+    ruleToFieldIdx = dict()
+
+    idxToRemove = -1
+    while True:
+        for rule in ruleToFieldsIdx:
+            if idxToRemove == -1:
+                length = len(ruleToFieldsIdx[rule])
+                if length == 1:
+                    idxToRemove = ruleToFieldsIdx[rule].pop()
+                    ruleToFieldIdx[rule] = idxToRemove
+                    break
+            else:
+                ruleToFieldsIdx[rule].remove(idxToRemove)
+        if idxToRemove == -1:
+            break
+        idxToRemove = -1
+
+    count = 1
+    for rule in ruleToFieldIdx:
+        if rule.startswith('departure'):
+            count *= myTicket[ruleToFieldIdx[rule]]
+
+
+    return count
 
 if test:
     lines = com.readFile("test.txt")
