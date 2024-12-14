009e265116ef80c8a9d3d95dff5713c26521552d
Cameron Warner <cameroncwarner@gmail.com>
2024-12-14 14:03:12 -0500

    Finish 2024 day1

--- src/2024/01/day1.py
+++ src/2024/01/day1.py
@@ -2,7 +2,7 @@ from aocd.get import get_day_and_year
 from aocd.models import Puzzle
 import common as com
 
-runExamples = True
+runExamples = False
 day, year = get_day_and_year()
 puzzle = Puzzle(year, day)
 
@@ -13,26 +13,55 @@ def inputParser2(input_data):
     return inputParser1(input_data)
 
 def Part1(data):
-    return None
+    lefts = []
+    rights = []
+    for line in data:
+        left, right = line.split('  ')
+        lefts.append(int(left))
+        rights.append(int(right))
+    
+    lefts.sort()
+    rights.sort()
+
+    distances = []
+    for x in range(lefts.__len__()):
+        distances.append(abs(lefts[x] - rights[x]))
+    return sum(distances)
 
 def Part2(data):
-    return None
+    lefts = []
+    rights = []
+    for line in data:
+        left, right = line.split('  ')
+        lefts.append(int(left))
+        rights.append(int(right))
+    
+    lefts.sort()
+    rights.sort()
 
+    distances = []
+    for x in lefts:
+        distances.append(x*rights.count(x))
+    return sum(distances)
+ 
 if runExamples:
     part1ExamplePassed = False
     part2ExamplePassed = False
     for i, example in enumerate(puzzle.examples):
-        part1Answer = Part1(inputParser1(example.input_data))
+        part1Answer = str(Part1(inputParser1(example.input_data)))
         if part1Answer != example.answer_a:
             print(f"Incorrect: part1 example {i}. Extra={example.extra};Input=\n{example.input_data}\nExpect: {example.answer_a}\nActual: {part1Answer}")
             part1ExamplePassed = False
             break
+        
+        print("Part 1 Example Passed")
         part1ExamplePassed = True
-        part2Answer = Part2(inputParser2(example.input_data))
+        part2Answer = str(Part2(inputParser2(example.input_data)))
         if part2Answer != example.answer_b:
             print(f"Incorrect: part2 example {i}. Extra={example.extra};Input=\n{example.input_data}\nExpect: {example.answer_b}\nActual: {part2Answer}")
             part2ExamplePassed = False
             break
+        print("Part 2 Example Passed")
         part2ExamplePassed = True
 else:
     part1ExamplePassed = True
--- src/template.py
+++ src/template.py
@@ -22,17 +22,20 @@ if runExamples:
     part1ExamplePassed = False
     part2ExamplePassed = False
     for i, example in enumerate(puzzle.examples):
-        part1Answer = Part1(inputParser1(example.input_data))
+        part1Answer = str(Part1(inputParser1(example.input_data)))
         if part1Answer != example.answer_a:
             print(f"Incorrect: part1 example {i}. Extra={example.extra};Input=\n{example.input_data}\nExpect: {example.answer_a}\nActual: {part1Answer}")
             part1ExamplePassed = False
             break
+        
+        print("Part 1 Example Passed")
         part1ExamplePassed = True
-        part2Answer = Part2(inputParser2(example.input_data))
+        part2Answer = str(Part2(inputParser2(example.input_data)))
         if part2Answer != example.answer_b:
             print(f"Incorrect: part2 example {i}. Extra={example.extra};Input=\n{example.input_data}\nExpect: {example.answer_b}\nActual: {part2Answer}")
             part2ExamplePassed = False
             break
+        print("Part 2 Example Passed")
         part2ExamplePassed = True
 else:
     part1ExamplePassed = True
