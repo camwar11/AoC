7cbb0bf6f03fdc6128859ac9dc79c68ae7f415c8
Cameron Warner <cameroncwarner@gmail.com>
2024-12-14 13:42:02 -0500

    Start 2024 day 1

--- createDay.sh
+++ createDay.sh
@@ -1,7 +1,8 @@
 #!/bin/sh 
+day="day$2"
 mkdir ./src/$1/$2
-cp ./src/template.py ./src/$1/$2/$2.py
-code ./src/$1/$2/$2.py
-touch ./src/$1/$2/input.txt
-touch ./src/$1/$2/test.txt
-code ./src/$1/$2/test.txt
+cp ./src/template.py ./src/$1/$2/$day.py
+# code ./src/$1/$2/$day.py
+# touch ./src/$1/$2/input.txt
+# touch ./src/$1/$2/test.txt
+# code ./src/$1/$2/test.txt
